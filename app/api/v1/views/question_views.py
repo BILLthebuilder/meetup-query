b477e95f3b3c3e31f435d85d32a89edecef4adfa
Bill <billkariri@gmail.com>
2019-01-10 07:18:18 +0300

    [finishes #163056051] finish creating the post questions endpoint

--- app/api/v1/views/question_views.py
+++ app/api/v1/views/question_views.py
@@ -23,3 +23,37 @@ def create_question():
     meetup = data.get('meetup')
     createdby = data.get('createdby')
     votes = data.get('votes')
+
+    if not title:
+        return make_response(jsonify({
+            "status": 400,
+            "message": "Please provide a topic"
+        })), 400
+
+    if not body:
+        return make_response(jsonify({
+            "status": 400,
+            "message": "Specify the location"
+        })), 400
+
+    if not meetup:
+        return make_response(jsonify({
+            "status": 400,
+            "message": "Please provide the meetup that you are asking for"
+        })), 400
+
+    if not createdby:
+        return make_response(jsonify({
+            "status": 400,
+            "message": "Please specify the creator"
+        })), 400
+    question = questions.create_question(
+            title, body, meetup, createdby, votes)
+    return make_response(jsonify({
+        "status": 201,
+        "data": [{
+            "title": title,
+            "body": body,
+            "meetup": meetup,
+            "user": createdby,
+        }]})), 201
