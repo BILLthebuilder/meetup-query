762c0a61bdce207efddd02a91aa8fa6ebebf3dd9
Bill <billkariri@gmail.com>
2019-01-10 09:12:48 +0300

    [feature #163109692] create method for the authentication token

--- app/api/v1/models/user_models.py
+++ app/api/v1/models/user_models.py
@@ -9,4 +9,10 @@ class UserModel(object):
     def __init__(self):
         self.users = USERS
     
-    
+    def generate_auth_token(self, username):
+        """ Method for auth token """
+        try:
+            payload = {'exp': datetime.utcnow() + timedelta(days=0, seconds=120), 'iat': datetime.utcnow(), 'sub': username}
+            return jwt.encode(payload, os.getenv('SECRET_KEY', 'my_secret'), algorithm='HS256').decode('utf-8')
+        except Exception as e:
+            return e
