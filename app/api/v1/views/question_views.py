13c68b05f3f01f69e9dc153501a1635279636633
Bill <billkariri@gmail.com>
2019-01-10 07:04:53 +0300

    [feature #163056051] get the data fields for the questions

--- app/api/v1/views/question_views.py
+++ app/api/v1/views/question_views.py
@@ -18,42 +18,4 @@ def create_question():
             "message": "Wrong input"
         })), 400
 
-    title = data.get('title')
-    body = data.get('body')
-    meetup = data.get('meetup')
-    createdby = data.get('createdby')
-    votes = data.get('votes')
-
-    if not title:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Please provide a topic"
-        })), 400
-
-    if not body:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Specify the location"
-        })), 400
-
-    if not meetup:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Please provide the meetup that you are asking for"
-        })), 400
-
-    if not createdby:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Please specify the creator"
-        })), 400
-    question = questions.create_question(
-            title, body, meetup, createdby, votes)
-    return make_response(jsonify({
-        "status": 201,
-        "data": [{
-            "title": title,
-            "body": body,
-            "meetup": meetup,
-            "user": createdby,
-        }]})), 201
+    
