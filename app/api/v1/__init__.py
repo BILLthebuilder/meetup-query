b5a8f76629ae5d68f2809113b43057844ec82d7c
Bill <billkariri@gmail.com>
2019-01-14 00:01:35 +0300

    [feature #163144547] add routes for creating a meetup

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,10 +1,10 @@
 from flask_restful import Api
 from flask import Blueprint
 
-version_one = Blueprint('api v1', __name__, url_prefix='/api/v1')
+version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
 api = Api(version_one)
 
-from .views.meetup_views import Meetup, Meetups
+from .views.meetup_views import Meetup
 
-api.add_resource(Meetup, '/meetups')
+api.add_resource(Meetup, '/meetups') #route to create a meetup record
