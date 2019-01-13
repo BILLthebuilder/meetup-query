019cac5352d4b5f490b3ea1c9c766534b25d929f
Bill <billkariri@gmail.com>
2019-01-14 00:02:52 +0300

    [finishes #163144547] post meetups endpoint using flask-restful

--- app/api/v1/views/meetup_views.py
+++ app/api/v1/views/meetup_views.py
@@ -1,54 +1,18 @@
-from flask import Flask, Blueprint, request, make_response, jsonify
-from ..models import meetup_models
-from werkzeug.exceptions import BadRequest
+from flask import jsonify, make_response, request
+from flask_restful import Resource
+from ..models.meetup_models import MeetupData
 
-meetupv1 = Blueprint('meetups', __name__, url_prefix='/meetups')
-meetups = meetup_models.MeetUpModel()
+class Meetup(MeetupData, Resource):
+    """ Defining the class for the meetup """
+    def __init__(self):
+        self.records = MeetupData()
 
-
-@meetupv1.route('', methods=['POST'])
-def create_meetup():
-    """ An endpoint to create meetups """
-
-    try:
+    def post(self):
+        """ The endpoint for creating a meetup entry """
         data = request.get_json()
-    except:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Wrong input"
-        })), 400
-    topic = data.get('topic')
-    location = data.get('location')
-    images = data.get('images')
-    happeningOn = data.get('happeningOn')
-    tags = data.get('tags')
-
-    if not topic:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Provide a topic"
-        })), 400
-    if not location:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Specify the location"
-        })), 400
-    if not happeningOn:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Provide a date"
-        })), 400
-    if not tags:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Provide a tag"
-        })), 400
-    else:
-        meetup = meetups.create_meetup(
-            topic, location, images, happeningOn, tags)
-        return make_response(jsonify({
-            "status": 201,
-            "data": [{"topic": topic,
-                      "location": location,
-                      "happeningOn": happeningOn,
-                      "tags": tags}]})), 201
+        title = data['Title']
+        description = data['Description']
+        date = data['Date']
+        location = data['Location']
+        resp = self.records.save(title, description, date, location)
+        return make_response(jsonify({"The Meetup is": resp}), 201)
