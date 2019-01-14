3b21d619428fc555189f8925e78897258396e5bf
Bill <billkariri@gmail.com>
2019-01-14 04:16:35 +0300

    [finishes #163055428] add route to get specific meetup

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -5,7 +5,8 @@ version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
 api = Api(version_one)
 
-from .views.meetup_views import Meetup
+from .views.meetup_views import Meetup, Meetups
 
 api.add_resource(Meetup, '/meetups') #route for POST and GET for meetup records
+api.add_resource(Meetups,'/meetups/<int:id>') # route to GET a specific meetup
 
