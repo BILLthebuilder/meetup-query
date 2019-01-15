6a8d2a7b2a73f969aaddbe94ec970e3650e20de5
Bill <billkariri@gmail.com>
2019-01-15 10:49:50 +0300

    [feature #163215356] improve formatting with autopep8

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,3 +1,6 @@
+from .views.rsvp_views import Reservations
+from .views.question_views import Question
+from .views.meetup_views import Meetup, Meetups
 from flask_restful import Api
 from flask import Blueprint
 
@@ -5,10 +8,10 @@ version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
 api = Api(version_one)
 
-from .views.meetup_views import Meetup, Meetups
-from .views.question_views import Question
 
-api.add_resource(Meetup, '/meetups') #route that serves both POSTing and GETting meetup records
-api.add_resource(Meetups,'/meetups/<int:id>') # route to GET a specific meetup
-api.add_resource(Question,'/questions') #route for POST question
-
+# route that serves both POSTing and GETting meetup records
+api.add_resource(Meetup, '/meetups')
+# route to GET a specific meetup
+api.add_resource(Meetups, '/meetups/<int:id>')
+# route for POST question
+api.add_resource(Question, '/questions')
