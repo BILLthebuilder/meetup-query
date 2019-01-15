49405f4f1ef3cefb5f60f14c1129c01e77c7fa49
Bill <billkariri@gmail.com>
2019-01-15 10:22:57 +0300

    [feature #163215356] add the reservations models

---
+++ app/api/v1/models/rsvp_models.py
@@ -0,0 +1,23 @@
+import datetime
+
+RESERVATIONS = []
+
+
+class QuestionsModel():
+    """ Mapping reservations and data relations """
+
+    def __init__(self):
+        self.reservations = RESERVATIONS
+
+    def save(self, topic, status):
+        """ The reservations db """
+
+        reserves = {
+            "topic": topic,
+            "createdOn": datetime.datetime.now(),
+            "id": len(self.reservations) + 1,
+            "status": status
+        }
+
+        self.reservations.append(reserves)
+        return self.reservations
