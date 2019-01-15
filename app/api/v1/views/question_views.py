5cfa55d6b7a57393ed767d946950ba6ce74f5911
Bill <billkariri@gmail.com>
2019-01-15 06:09:22 +0300

    [feature #163180923] Create endpoint for posting a question

--- app/api/v1/views/question_views.py
+++ app/api/v1/views/question_views.py
@@ -1,59 +1,18 @@
-from flask import Flask, Blueprint, request, make_response, jsonify
-from ..models import question_models
-from werkzeug.exceptions import BadRequest
+from flask import jsonify, make_response, request
+from flask_restful import Resource
+from ..models.question_models import QuestionsModel
 
-questionsv1 = Blueprint('questions', __name__, url_prefix='/api/v1/questions')
-questions = question_models.QuestionModel()
+class Question(QuestionsModel, Resource):
+    """ Posting the question """
+    def __init__(self):
+        self.questions_records = QuestionsModel()
 
-
-@questionsv1.route('', methods=['POST'])
-def create_question():
-    """ An Endpoint for creating a question about a meetup """
-
-    try:
+    def post(self):
+        """ The endpoint for posting a question """
         data = request.get_json()
-    except:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Wrong input"
-        })), 400
-
-    title = data.get('title')
-    body = data.get('body')
-    meetup = data.get('meetup')
-    createdby = data.get('createdby')
-    votes = data.get('votes')
-
-    if not title:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Please provide a topic"
-        })), 400
-
-    if not body:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Specify the location"
-        })), 400
-
-    if not meetup:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Please provide the meetup that you are asking for"
-        })), 400
-
-    if not createdby:
-        return make_response(jsonify({
-            "status": 400,
-            "message": "Please specify the creator"
-        })), 400
-    question = questions.create_question(
-            title, body, meetup, createdby, votes)
-    return make_response(jsonify({
-        "status": 201,
-        "data": [{
-            "title": title,
-            "body": body,
-            "meetup": meetup,
-            "user": createdby,
-        }]})), 201
+        question = data['question']
+        resp = self.questions_records.save(question)
+        
+        if resp is not None:
+            return make_response(jsonify({"The Questions are": resp}), 201)
+        return make_response(jsonify({"Error": "Question could not be posted"}), 500)
