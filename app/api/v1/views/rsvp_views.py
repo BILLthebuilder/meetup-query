c40056abe7baa34921a24f7f3c1d4a5bf770b5be
Bill <billkariri@gmail.com>
2019-01-15 13:50:05 +0300

    [finishes #163215356] create the views for meetup reservations

---
+++ app/api/v1/views/rsvp_views.py
@@ -0,0 +1,25 @@
+from flask import jsonify, make_response, request
+from flask_restful import Resource
+from ..models.rsvp_models import RsvpModel
+
+
+class Reservations(RsvpModel, Resource):
+    """ Posting the reservation """
+
+    def __init__(self):
+        self.reserve_records = RsvpModel()
+
+    def post(self, id):
+        """ The endpoint to rsvp to a meetup"""
+        data = request.get_json()
+        topic = data['topic']
+        status = data['status']
+        resp = self.reserve_records.save(id, topic, status)
+
+        if resp is not None:
+            return make_response(jsonify({
+                "Status": 201,
+
+                "Here is your reservation":
+                resp}), 201)
+        return make_response(jsonify({"Error": "Unable to book reservation"}), 500)
