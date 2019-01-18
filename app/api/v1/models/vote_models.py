eb8d71a8d4bb3ebe65b2d9da4871f6d27be6ff50
Bill <billkariri@gmail.com>
2019-01-18 10:03:31 +0300

    Update vote models

--- app/api/v1/models/vote_models.py
+++ app/api/v1/models/vote_models.py
@@ -12,34 +12,35 @@ class Votes():
 
     def find_vote(self, id):
         response = None
-        for each_item in self.vote_records:
-            if each_item['question_id'] == id:
-                response = each_item
-        return response
-
-    def vote(self, id, vote):
-        record = self.find_vote(id)
-        if record is not None:
-            record['upvotes'] += 1
-            return record
-        else:
-            response = self.save(id, vote)
+        for item in self.vote_records:
+            if item['meetup_id'] == id:
+                response = item
         return response
 
     def save(self, id, vote):
 
         data = {
-            "question_id": id,
-            "upvotes": 0,
-            "downvotes": 0,
+            "meetup_id": id,
+            "votes": 0,
             "voted_on": datetime.datetime.now()
         }
-        # Checking for upvotes
-        if vote:
-            data['upvotes'] += 1
 
-        # Checking for downvotes
-        else:
-            data['downvotes'] += 1
+        if vote:
+            data['votes'] += 1
+
+        elif data['votes'] > 0:
+            data['downvotes'] -= 1
         self.vote_records.append(data)
         return self.vote_records
+
+    def vote(self, id, vote):
+        record = self.find_vote(id)
+        if record is not None:  # Record exists...
+            if vote:
+                record['votes'] += 1
+            elif record['votes'] > 0:
+                record['votes'] -= 1
+            return record
+        else:  # record does not exists
+            response = self.save(id, vote)
+        return response
